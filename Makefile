f14992076d48e9ddf7aa7f3a85d02be265ea285f
Denis Merigoux <denis.merigoux@gmail.com>
2022-06-03 11:05:54 +0200

    Draft of interface with @R1kM and @rmonat

---
+++ Makefile
@@ -0,0 +1,2 @@
+build:
+	dune build lib/
---
+++ lib/dates.mli
@@ -0,0 +1,60 @@
+(* This file is part of the Dates_calc library. Copyright (C) 2022 Inria,
+   contributors: Denis Merigoux <denis.merigoux@inria.fr>, Aymeric Fromherz
+   <aymeric.fromherz@inria.fr>, Raphaël Monat <raphael.monat@lip6.fr>
+
+   Licensed under the Apache License, Version 2.0 (the "License"); you may not
+   use this file except in compliance with the License. You may obtain a copy of
+   the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+   License for the specific language governing permissions and limitations under
+   the License. *)
+
+type date
+(** A valid date in the standard Gregorian calendar. *)
+
+type period
+(** A period can be any number and combination of days, months, years. *)
+
+type day
+type month
+type year
+
+exception InvalidDate
+exception AmbiguousComputation
+
+type date_rounding =
+  | RoundUp
+  | RoundDown
+  | AbortOnRound
+      (** When choosing [AbortOnRound], functions may raise
+          [AmbiguousComputation]. *)
+
+(** {2 Functions on dates}*)
+
+val make_date : year:int -> month:int -> day:int -> date
+(** @raise [InvalidDate]*)
+
+val add_dates : ?round:date_rounding -> date -> period -> date
+(** @raise [AmbiguousComputation] *)
+
+val sub_dates : date -> date -> period
+(** The returned [period] is always expressed as a number of days. *)
+
+val compare_dates : date -> date -> int
+val date_to_ymd : date -> int * int * int
+
+(** {2 Functions on periods}*)
+
+val make_period : years:int -> months:int -> days:int -> period
+val add_periods : period -> period -> period
+val sub_periods : period -> period -> period
+val mul_period : period -> int -> period
+
+val period_to_days : period -> int
+(** @raise [AmbiguousComputation]
+      when the period is anything else than a number of days. *)
