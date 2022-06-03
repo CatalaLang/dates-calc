e3f799e30126ebd17e667b99bd45f4554d158f1d
Denis Merigoux <denis.merigoux@gmail.com>
2022-06-03 11:50:25 +0200

    First implem of first hard function

--- lib/dates.ml
+++ lib/dates.ml
@@ -34,7 +34,7 @@ type date_rounding =
 
 (** {2 Functions on dates}*)
 
-let is_leap_year ~(year : int) : bool =
+let is_leap_year (year : int) : bool =
   year mod 400 = 0 || (year mod 4 = 0 && year mod 100 <> 0)
 
 (** @raise [InvalidDate]*)
@@ -46,7 +46,7 @@ let days_in_month ~(month : int) ~(is_leap_year : bool) : int =
   | _ -> raise InvalidDate
 
 let is_valid_date ~(year : int) ~(month : int) ~(day : int) : bool =
-  try day >= 1 && day <= days_in_month ~month ~is_leap_year:(is_leap_year ~year)
+  try day >= 1 && day <= days_in_month ~month ~is_leap_year:(is_leap_year year)
   with InvalidDate -> false
 
 (** @raise [InvalidDate]*)
@@ -54,10 +54,61 @@ let make_date ~(year : int) ~(month : int) ~(day : int) : date =
   if is_valid_date ~year ~month ~day then { year; month; day }
   else raise InvalidDate
 
-(** @raise [AmbiguousComputation] *)
-let add_dates ?(round : date_rounding = AbortOnRound) (d1 : date) (d2 : period)
-    : date =
+let add_dates_years ~(round : date_rounding) (d : date) (years : int) =
   failwith "Unimplemented!"
+
+let add_dates_month ~(round : date_rounding) (d : date) (months : int) =
+  failwith "Unimplemented!"
+
+(** Returns new [year, month]. Precondition: [1 <= month <= 12] *)
+let rec add_months_to_first_of_month_date
+    ~(year : int)
+    ~(month : int)
+    ~(months : int) : int * int =
+  let new_month = month + months in
+  if 1 <= new_month && new_month <= 12 then year, new_month
+  else if new_month > 12 then
+    add_months_to_first_of_month_date ~year:(year + 1) ~month
+      ~months:(months - 12)
+  else
+    (* new_month <= 0 *)
+    add_months_to_first_of_month_date ~year:(year - 1) ~month
+      ~months:(months + 12)
+
+let rec add_dates_days (d : date) (days : int) =
+  let days_in_d_month =
+    days_in_month ~month:d.month ~is_leap_year:(is_leap_year d.year)
+  in
+  let new_day = d.day + days in
+  if 1 <= new_day && new_day <= days_in_d_month then { d with day = new_day }
+  else if new_day >= days_in_d_month then
+    let new_year, new_month =
+      add_months_to_first_of_month_date ~year:d.year ~month:d.month ~months:1
+    in
+    add_dates_days
+      { year = new_year; month = new_month; day = 1 }
+      (days - (days_in_d_month - d.day)
+     - 1 (* we already moved one day by incrementing the month *))
+  else
+    let new_year, new_month =
+      add_months_to_first_of_month_date ~year:d.year ~month:d.month ~months:(-1)
+    in
+    add_dates_days
+      {
+        year = new_year;
+        month = new_month;
+        day =
+          days_in_month ~month:new_month ~is_leap_year:(is_leap_year new_year);
+      }
+      (days + d.day)
+
+(** @raise [AmbiguousComputation] *)
+let add_dates ?(round : date_rounding = AbortOnRound) (d : date) (p : period) :
+    date =
+  let d = add_dates_years ~round d p.years in
+  let d = add_dates_month ~round d p.years in
+  let d = add_dates_days d p.years in
+  d
 
 (** The returned [period] is always expressed as a number of days. *)
 let sub_dates (d1 : date) (d2 : date) : period = failwith "Unimplemented!"
